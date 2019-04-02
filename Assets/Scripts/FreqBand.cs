23e33c8185ed06ae07f847351dfa39bcf715a306
deej <25407341+DjOlaiya@users.noreply.github.com>
2019-04-01 21:53:25 -0700

    multidimensional array works, Need a more efficient Algo

--- Assets/Scripts/FreqBand.cs
+++ Assets/Scripts/FreqBand.cs
@@ -8,10 +8,14 @@ public class FreqBand: MonoBehaviour
     public float _Mult,_startvalue;
     public static int N =InitAudio.numBands;
     GameObject[] _mybands = new GameObject[N];
-    Transform[] _bandarray= new Transform[N];
+    //Transform[] _bandarray= new Transform[N];
+    Transform [,] _bandarray2d = new Transform[N, N];
      public  float[] _prevfreqband = new float[N];
     
     float[] _CurFreqBand = new float[N];
+
+    public float[,] _ampArr2d = new float[N,N];
+    public static int time; 
     
     void Start()
     {
@@ -26,51 +30,101 @@ public class FreqBand: MonoBehaviour
         Vector3 scale= Vector3.one*stepSize;
         Vector3 position;
         position.y=0;
-        position.z=0;
+        time = 0;
 
-         for(int i =0,z=0;i<N;z++)
-            {
-                for(int x=0;x<N;x++,i++)
-                {
-                 Transform _spect=Instantiate(prefab);
-                    position.x=i;
-                    _spect.localPosition=position;
-                    _spect.localScale=scale;
-                    _spect.name="shape"+i;
-                    _spect.SetParent(transform);
-                    _bandarray[i]= _spect;
-                }    
+        for(int i=0; i<N; i++){
+            for(int j=0;j<N; j++){
+                _ampArr2d[i,j] = 1;
             }
-        //  for(int i=0; i<N; i++)
-        // {
-        //     GameObject _SpectralBand1024 = (GameObject)Instantiate(_SpectralBands);
-        //     _SpectralBand1024.transform.position = this.transform.position;
-        //     _SpectralBand1024.transform.parent=this.transform;
-        //     _SpectralBand1024.name="spectralBandwidth" + i;
-        //     _SpectralBand1024.transform.position=new Vector3(i,i,10);
-        //     _mybands[i] = _SpectralBand1024;
-        // }
+        }
+
+
+        // for(int i=0;i<N;i++)
+        //         {
+        //          Transform _spect=Instantiate(prefab);
+        //             position.x=i;
+        //             _spect.localPosition=position;
+        //             _spect.localScale=scale;
+        //             _spect.name="shape"+i;
+        //             _spect.SetParent(transform);
+        //             _bandarray[i]= _spect;
+        //         }    
+        //  for(int i =0,z=0;i<N;z++)
+        //     {
+        //         for(int x=0;x<N;x++,i++)
+        //         {
+        //          Transform _spect=Instantiate(prefab);
+        //             position.x=i;
+        //             _spect.localPosition=position;
+        //             _spect.localScale=scale;
+        //             _spect.name="shape"+i;
+        //             _spect.SetParent(transform);
+        //             _bandarray[i]= _spect;
+        //         }    
+        //     }
+
+
+        for(int i = 0; i< N; i++){
+            for(int j = 0; j<N; j++){
+                Transform _spect = Instantiate(prefab);
+                position.x = j;
+                position.z = i;
+                _spect.localPosition = position;
+                _spect.localScale = scale;
+                _spect.SetParent(transform);
+                _bandarray2d[i,j] = _spect;
+
+            }
+        }
+     
     }
   
     void Update()
     {
         BandAmpFn();
+        //_CurFreqBand.CopyTo(_prevfreqband,0);
+           // spectra.CopyTo(_CurFreqBand,0); 
     }
 
 //creates the dynamic change in the shapes along y axis
     void BandAmpFn()
     {
-        for(int i=0;i<N;i++)
-        {
-                _bandarray[i].transform.localScale = new Vector3(transform.localScale.x,
-                (InitAudio._freqGroupd[i]*_Mult)+_startvalue,transform.localScale.z);
-        }
+        // for(int i=0;i<N;i++)
+        // {
+        //         _bandarray[i].transform.localScale = new Vector3(transform.localScale.x,
+        //         (InitAudio._freqGroupd[i]*_Mult)+_startvalue,transform.localScale.z);
+        // }
         
         // for(int i=0;i<N;i++)
         // {
         //         _mybands[i].transform.localScale = new Vector3(transform.localScale.x,
         //         (InitAudio._freqGroupd[i]*_Mult)+_startvalue,transform.localScale.z);
         // }
+
+        for(int i = 0; i<N;i++){
+            for(int j = 1; j<N; j++){
+                _ampArr2d[i,j] = _ampArr2d[i,j-1];
+            }
+        }
+
+        for(int x = 0; x<N; x++){
+            _ampArr2d[x, 0] = InitAudio._freqGroupd[x];
+            //time ++;
+        }
+
+
+        // for(int i=0; i<N; i++){
+        //     for(int j=0; j<N; j++){
+        //         _bandarray2d[i,j].transform.localScale = new Vector3(transform.localScale.x,
+        //         (InitAudio._freqGroupd[j]*_Mult)+_startvalue,transform.localScale.z);
+        //     }
+        // }
+        for(int i=0; i<N; i++){
+            for(int j=0; j<N; j++){
+                _bandarray2d[i,j].transform.localScale = new Vector3(transform.localScale.x,
+                (_ampArr2d[j,i]*_Mult)+_startvalue,transform.localScale.z);
+            }
+        }
     }
    
     //take and store old sample values to use graph.  
--- Assets/Scripts/InitAudio.cs
+++ Assets/Scripts/InitAudio.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 public class InitAudio : MonoBehaviour
 {
     //all my declarations
-     public const int numBands=512;
+     public const int numBands=32;
      public const int numSamples=1024;
     AudioSource  _audio;
     public static float[] _sample = new float[numSamples]; //once i make it static it wont show up in unity cos it cant be changed
@@ -36,7 +36,7 @@ public class InitAudio : MonoBehaviour
  void GroupFreq()
     {
         /*
-        220050/1024= 5.38hz/sample
+        220050/1024= 5.38hz/sample_el
         split this into 1024 bands. If I do it evenly then 21.53hz/band. 
         If I try to do it using powers of 2 like he did, I end up with
         really small values in the range 0-1 for the first few values.
@@ -45,14 +45,15 @@ public class InitAudio : MonoBehaviour
         int sampleID =0;
         for(int i=0;i<numBands;i++)
         {
-            int samplePerBand= 8;
+            int samplePerBand= 32;
             float avg=0;
             for(int j=0;j<samplePerBand;j++)
             {
-                avg+= _sample[sampleID]*(sampleID+1);
+                avg+= _sample[sampleID]; //*(sampleID+1);
                 sampleID++;
             }
             avg/=samplePerBand;
+        //valuesin arr are small so mult by 10 to make
             _freqGroupd[i]=avg*10;
     }
 
